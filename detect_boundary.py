92c5ecb018c171382571cd6d0dc9575a94923e3f
surya acuru <suryasaradhi3@gmail.com>
2020-07-31 14:24:59 +0530

    Added examples shapes

--- detect_boundary.py
+++ detect_boundary.py
@@ -6,21 +6,21 @@ ret,thresh = cv2.threshold(gray,127,255,1)
 contours,h = cv2.findContours(thresh,1,2)
 for cnt in contours:
     approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-    print len(approx)
+    print(len(approx))
     if len(approx)==5:
-        print "pentagon"
+        print("pentagon")
         cv2.drawContours(img,[cnt],0,255,-1)
     elif len(approx)==3:
-        print "triangle"
+        print("triangle")
         cv2.drawContours(img,[cnt],0,(0,255,0),-1)
     elif len(approx)==4:
-        print "square"
+        print ("square")
         cv2.drawContours(img,[cnt],0,(0,0,255),-1)
     elif len(approx) == 9:
-        print "half-circle"
+        print ("half-circle")
         cv2.drawContours(img,[cnt],0,(255,255,0),-1)
     elif len(approx) > 15:
-        print "circle"
+        print("circle")
         cv2.drawContours(img,[cnt],0,(0,255,255),-1)
 cv2.imshow('img',img)
 cv2.waitKey(0)
